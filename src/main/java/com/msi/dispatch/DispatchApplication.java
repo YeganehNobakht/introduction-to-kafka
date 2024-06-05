72aaf42d7bab22827219082747eb0849548ba3fc
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2024-06-05 05:02:49 -0700

    bug fix

--- src/main/java/com/msi/dispatch/DispatchApplication.java
+++ src/main/java/com/msi/dispatch/DispatchApplication.java
@@ -3,11 +3,14 @@ package com.msi.dispatch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class DispatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DispatchApplication.class, args);
+		System.out.println(UUID.randomUUID());
 	}
 
 }
